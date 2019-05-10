3be3f42012885aeaea7380816b25e25370b00b39
Tom Parke <tom.parke15@gmail.com>
2019-05-10 19:07:47 +1000

    Backgrounda and animation
    Primitive versions of both added. There's some javascript in here now, too.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let entertain = document.getElementById('entertain')
+
+entertain.addEventListener("click", displayToggle)
+
+function displayToggle() {
+    var x = document.getElementById("special");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    console.log('clicked')
+  }
