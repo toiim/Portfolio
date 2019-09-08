019d4c6f55e87c5b291ce64bfc908e6646c6c6c1
Tom Parke <tom.parke15@gmail.com>
2019-09-08 23:40:13 +1000

    Totally Different
    I just went and redid the whole site basically.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-let entertain = document.getElementById('entertain')
+let entertain = document.getElementById("entertain")
 let special = document.getElementById("special")
-
+debugger
 entertain.addEventListener("click", displayToggle)
 
 function displayToggle() {
-    if (special.style.display == "none") {
-      special.style.display = "block";
-    } else {
-      special.style.display = "none";
-    }
-    console.log('clicked')
+  if (special.style.display == "none") {
+    special.style.display = "block"
+  } else {
+    special.style.display = "none"
   }
-
+  debugger
+}
